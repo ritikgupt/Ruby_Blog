826692b4c70323a2a4b0c9b41982aa1ce9fadc43
ritikgupt <ritik.gupta2018@vitstudent.ac.in>
2020-10-30 18:58:27 +0530

    Basic rails blog app done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   get 'welcome/about',to: 'welcome#about'
-  resources :articles
+  resources :articles do
+  resources :comments
+  end
 end
